d41b02bf0f6c302459114403c2625a3cf47734cf
Demetrius Vignati <demetriusvas@gmail.com>
2022-02-16 11:56:27 -0300

    separei o css e o js

---
+++ script.js
@@ -0,0 +1,11 @@
+function acao_botao(){
+    alert("Botao foi clicado");
+}
+
+function acao(){
+    alert("Elemento foi clicado");
+}
+
+function acao_botao_dbl(){
+    alert("Double Clique foi clicado");
+}
